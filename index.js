4afe336456219e32b4b3df9026e83a21505de03a
Andrew Usher <andrewusher00@gmail.com>
2020-08-09 09:23:32 -0500

    refactor(config): move to react-hooks recommended
    BREAKING CHANGE: exhaustive-deps is warn instead of error now

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const OFF = 0
-const WARN = 1
-const ERROR = 2
-
 module.exports = {
   extends: [
     'standard',
     'plugin:jsx-a11y/recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
   ],
-  plugins: ['jsx-a11y', 'react', 'react-hooks'],
-  rules: {
-    'react-hooks/rules-of-hooks': 2,
-    'react-hooks/exhaustive-deps': 2
-  }
+  plugins: ['jsx-a11y', 'react', 'react-hooks']
 }
