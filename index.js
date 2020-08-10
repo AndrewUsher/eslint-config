2a15b254fa67b44dc9e58db45d78bb5bf3504848
Andrew Usher <andrewusher00@gmail.com>
2020-08-10 18:00:37 -0500

    refactor: split out base/react config
    BREAKING CHANGE: import for config needs to be changed

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 module.exports = {
-  extends: [
-    'standard',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
-  ],
-  plugins: ['jsx-a11y', 'react', 'react-hooks']
+  extends: ['standard']
 }
---
+++ react.js
@@ -0,0 +1,16 @@
+const OFF = 0
+const WARNING = 1
+const ERROR = 2
+
+module.exports = {
+  extends: [
+    require.resolve('./index.js'),
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['jsx-a11y', 'react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': ERROR,
+    'react-hooks/exhaustive-deps': WARNING
+  }
+}
